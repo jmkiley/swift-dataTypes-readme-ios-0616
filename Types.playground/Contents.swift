9e138ac47f77943ad19f979bf7b525e7618950e7
jmkiley <jordankiley@gmail.com>
2016-07-05 14:51:14 -0400

    Done.

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -5,12 +5,15 @@ ninthPlanet = "Pluto"
 
 let fifthPlanet: String = "Jupiter"
 // fifthPlanet is a constant of type ______
+print("fifthPlanet \"\(fifthPlanet)\" is a string")
 
 let sixthPlanet: String = "Saturn"
 // sixthPlanet is a constant of type ______
+print("sixthPlanet \"\(sixthPlanet)\" is a string")
 
 let seventhPlanet = "Uranus"
 // seventhPlanet is a constant of type ______
+print("seventhPlanet \"\(seventhPlanet)\" is a string")
 
 let numberOfCountriesInAfrica = 54
 
